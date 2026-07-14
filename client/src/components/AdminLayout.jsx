2e5c0422a2a8f2682f772cd4a882838e91b36819
Thaveesha Vithana <t.s.vithana@gmail.com>
2026-07-14 23:51:18 +0530

    main content area

--- client/src/components/AdminLayout.jsx
+++ client/src/components/AdminLayout.jsx
@@ -82,7 +82,7 @@ export default function AdminLayout() {
         </div>
       </aside>
 
-      {/* MAIN CONTENT AREA */}
+      {/* main area */}
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden bg-gray-50/50">
         
         {/* UNIVERSAL TOP BAR */}
