55dfdf4d60059e2646b3c9bfe8106b4f30f2fb69
Thaveesha Vithana <t.s.vithana@gmail.com>
2026-07-21 21:27:05 +0530

    variants value 2

--- client/src/pages/admin/AdminProducts.jsx
+++ client/src/pages/admin/AdminProducts.jsx
@@ -27,7 +27,7 @@ export default function AdminProducts() {
 
     // Variants
     const [colors, setColors] = useState([]);
-    const [sizes, setSizes] = useState([]); // { value: 'XL', price: '', imageBase64: '' }
+    const [sizes, setSizes] = useState([]);
     const [customVariants, setCustomVariants] = useState([]); // { title: 'Watt', options: [...] }
     
     // UI States
